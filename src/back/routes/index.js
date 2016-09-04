e178546423ddb511acf41db731921eea1248c3a5
Amir Hoseinian <amir.anyware@gmail.com>
2016-09-04 21:36:06 +0430

    request in home page notification added

--- src/back/routes/index.js
+++ src/back/routes/index.js
@@ -1,9 +1,19 @@
 'use strict';
 var router = require('express').Router();
+var async = require('async');
+var Request = require('../models/request');
 
 
-router.get('/', function(req, res) {
-  res.render('index');
+router.get('/', function(req, res, next) {
+  async.parallel({
+    requestsCount: (cb) => {
+      if (!req.user) return cb(null, null);
+      Request.count({ _guild: req.user._guild._id, state: 0 }).populate('_user').exec(cb);
+    },
+  }, function(err, data) {
+    if (err) return next(err);
+    res.render('index', { data: data });
+  });
 });
 
 
