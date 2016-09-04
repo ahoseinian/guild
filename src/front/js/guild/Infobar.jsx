173352d4e90148d5e14ae8a4fd141736bac0b5ea
Amir Hoseinian <amir.anyware@gmail.com>
2016-09-05 00:00:26 +0430

    infobar style fix

--- src/front/js/guild/Infobar.jsx
+++ src/front/js/guild/Infobar.jsx
@@ -3,12 +3,18 @@ import {Card, CardText, CardBlock} from '../common/card.jsx';
 
 export const Infobar = props => (
   <div className="infobar">
-    <PublicInfo {...props}/>
-    <PrivateInfo {...props}/>
+    {props.guild.public
+      ? <PublicInfo guild={props.guild}/>
+      : null}
+
+    {props.guild.private
+      ? <PrivateInfo {...props}/>
+      : null}
   </div>
 );
 Infobar.propTypes = {
-  children: React.PropTypes.node
+  children: React.PropTypes.node,
+  guild: React.PropTypes.object.isRequired
 };
 
 const PublicInfo = props => {
@@ -18,9 +24,11 @@ const PublicInfo = props => {
         {props.guild._image
           ? <img src={props.guild._image.url} alt={props.guild.name} className="img-fluid"/>
           : null}
-        <CardBlock>
-          <CardText text={props.guild.public.text}/>
-        </CardBlock>
+        {props.guild.public.text
+          ? <CardBlock>
+              <CardText text={props.guild.public.text}/>
+            </CardBlock>
+          : null}
       </Card>
     )
     : null;
