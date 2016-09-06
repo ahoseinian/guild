af511b98ba5b2529de4397b3897e0ef284bc8263
Amir Hoseinian <amir.anyware@gmail.com>
2016-09-06 14:53:25 +0430

    realm route added

---
+++ src/back/routes/api/realms/index.js
@@ -0,0 +1,12 @@
+'use strict';
+var router = require('express').Router();
+var Realm = require('../../../models/realm');
+
+router.get('/:region', function(req, res, next){
+  Realm.find({region: req.params.region}).sort('-name').exec(function(err, items){
+    if(err) return next(err);
+    res.json(items);
+  });
+});
+
+module.exports = router;
